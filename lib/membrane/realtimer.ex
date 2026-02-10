6e4bfd59675cda3f1001d3175c204915715db4b1
noarkhh <jakub.pryc@gmail.com>
2026-02-10 16:27:52 +0100

    Add latency option

--- lib/membrane/realtimer.ex
+++ lib/membrane/realtimer.ex
@@ -14,100 +14,135 @@ defmodule Membrane.Realtimer do
   def_input_pad :input, accepted_format: _any, flow_control: :manual, demand_unit: :buffers
   def_output_pad :output, accepted_format: _any, flow_control: :push
 
+  def_options latency: [
+                spec: Membrane.Time.non_neg(),
+                default: 0,
+                description: """
+                Inital latency lol
+                """
+              ]
+
+  defmodule State do
+    @moduledoc false
+
+    @type t :: %__MODULE__{
+            reference_time: Membrane.Time.non_neg() | nil,
+            reference_timestamp: Membrane.Time.non_neg() | nil,
+            tick_actions: [Membrane.Element.Action.t()],
+            timer_status: :to_be_started | :running | :to_be_stopped
+          }
+
+    @enforce_keys [:latency]
+
+    defstruct @enforce_keys ++
+                [
+                  reference_time: nil,
+                  reference_timestamp: nil,
+                  tick_actions: [],
+                  timer_status: :to_be_started
+                ]
+  end
+
   @impl true
-  def handle_init(_ctx, _opts) do
-    {[], %{previous_timestamp: nil, tick_actions: [], timer_status: :to_be_started}}
+  def handle_init(_ctx, opts) do
+    {[], %State{latency: opts.latency}}
   end
 
   @impl true
   def handle_playing(_ctx, state) do
-    {[demand: {:input, 1}], state}
+    {[demand: :input], state}
   end
 
   @impl true
-  def handle_buffer(:input, buffer, _ctx, state) do
+  def handle_buffer(:input, buffer, _ctx, state = %State{}) do
     {maybe_start_timer, state} =
-      if state.timer_status == :to_be_started,
-        do: {[start_timer: {:timer, :no_interval}], %{state | timer_status: :running}},
-        else: {[], state}
-
-    state =
-      with %{previous_timestamp: nil} <- state do
-        %{state | previous_timestamp: Buffer.get_dts_or_pts(buffer) || 0}
+      if state.timer_status == :to_be_started do
+        {
+          [start_timer: {:timer, :no_interval}],
+          %State{
+            state
+            | timer_status: :running,
+              reference_time: Membrane.Time.monotonic_time(),
+              reference_timestamp: Buffer.get_dts_or_pts(buffer)
+          }
+        }
+      else
+        {[], state}
       end
 
-    interval = Buffer.get_dts_or_pts(buffer) - state.previous_timestamp
+    time_from_reference_time = Buffer.get_dts_or_pts(buffer) - state.reference_timestamp
 
-    state = %{
+    target_time = state.reference_time + time_from_reference_time + state.latency
+
+    interval = max(target_time - Membrane.Time.monotonic_time(), 0)
+
+    state = %State{
       state
-      | previous_timestamp: Buffer.get_dts_or_pts(buffer),
-        tick_actions: [buffer: {:output, buffer}] ++ state.tick_actions
+      | tick_actions: [buffer: {:output, buffer}] ++ state.tick_actions
     }
 
     {maybe_start_timer ++ [timer_interval: {:timer, interval}], state}
   end
 
   @impl true
-  def handle_event(:input, %Events.Reset{}, _ctx, state) do
-    {actions, state} =
-      case state.tick_actions do
-        [] when state.timer_status == :to_be_started ->
-          {[], state}
+  def handle_event(:input, %Events.Reset{}, _ctx, %State{} = state) do
+    case state.tick_actions do
+      [] when state.timer_status == :to_be_started ->
+        {[], state}
 
-        [] when state.timer_status == :running ->
-          {[stop_timer: :timer], %{state | timer_status: :to_be_started}}
+      [] when state.timer_status == :running ->
+        {[stop_timer: :timer], %State{state | timer_status: :to_be_started}}
 
-        _many ->
-          {[], %{state | timer_status: :to_be_stopped}}
-      end
-
-    {actions, %{state | previous_timestamp: nil}}
+      _many ->
+        {[], %State{state | timer_status: :to_be_stopped}}
+    end
   end
 
   @impl true
-  def handle_event(pad, event, _ctx, %{tick_actions: tick_actions} = state)
+  def handle_event(pad, event, _ctx, %State{tick_actions: tick_actions} = state)
       when pad == :output or tick_actions == [] do
     {[forward: event], state}
   end
 
   @impl true
-  def handle_event(:input, event, _ctx, state) do
-    {[], %{state | tick_actions: [event: {:output, event}] ++ state.tick_actions}}
+  def handle_event(:input, event, _ctx, %State{} = state) do
+    {[], %State{state | tick_actions: [event: {:output, event}] ++ state.tick_actions}}
   end
 
   @impl true
-  def handle_stream_format(:input, stream_format, _ctx, %{tick_actions: []} = state) do
+  def handle_stream_format(:input, stream_format, _ctx, %State{tick_actions: []} = state) do
     {[forward: stream_format], state}
   end
 
   @impl true
-  def handle_stream_format(:input, stream_format, _ctx, state) do
-    {[], %{state | tick_actions: [stream_format: {:output, stream_format}] ++ state.tick_actions}}
+  def handle_stream_format(:input, stream_format, _ctx, %State{} = state) do
+    {[],
+     %State{state | tick_actions: [stream_format: {:output, stream_format}] ++ state.tick_actions}}
   end
 
   @impl true
-  def handle_end_of_stream(:input, _ctx, %{tick_actions: []} = state) do
+  def handle_end_of_stream(:input, _ctx, %State{tick_actions: []} = state) do
     {[end_of_stream: :output], state}
   end
 
   @impl true
-  def handle_end_of_stream(:input, _ctx, state) do
-    {[], %{state | tick_actions: [end_of_stream: :output] ++ state.tick_actions}}
+  def handle_end_of_stream(:input, _ctx, %State{} = state) do
+    {[], %State{state | tick_actions: [end_of_stream: :output] ++ state.tick_actions}}
   end
 
   @impl true
-  def handle_tick(:timer, ctx, state) do
+  def handle_tick(:timer, ctx, %State{} = state) do
     actions =
       [timer_interval: {:timer, :no_interval}] ++
         Enum.reverse(state.tick_actions) ++
-        if ctx.pads.input.end_of_stream?, do: [], else: [demand: {:input, 1}]
+        if ctx.pads.input.end_of_stream?, do: [], else: [demand: :input]
 
     {maybe_stop_timer, state} =
       case state.timer_status do
-        :to_be_stopped -> {[stop_timer: :timer], %{state | timer_status: :to_be_started}}
+        :to_be_stopped -> {[stop_timer: :timer], %State{state | timer_status: :to_be_started}}
         :running -> {[], state}
       end
 
-    {actions ++ maybe_stop_timer, %{state | tick_actions: []}}
+    {actions ++ maybe_stop_timer, %State{state | tick_actions: []}}
   end
 end
--- test/membrane_realtimer_test.exs
+++ test/membrane_realtimer_test.exs
@@ -2,12 +2,11 @@ defmodule Membrane.RealtimerTest do
   use ExUnit.Case
 
   import Membrane.Testing.Assertions
+  import Membrane.ChildrenSpec
 
   alias Membrane.{Buffer, Realtimer, Testing, Time}
 
   test "Limits playback speed to realtime" do
-    import Membrane.ChildrenSpec
-
     buffers = [
       %Buffer{pts: 0, payload: 0},
       %Buffer{pts: Time.milliseconds(100), payload: 1}
@@ -21,6 +20,29 @@ defmodule Membrane.RealtimerTest do
 
     pipeline = Testing.Pipeline.start_link_supervised!(spec: spec)
 
+    assert_sink_buffer(pipeline, :sink, %Buffer{payload: 0})
+    refute_sink_buffer(pipeline, :sink, _buffer, 90)
+    assert_sink_buffer(pipeline, :sink, %Buffer{payload: 1}, 20)
+    assert_end_of_stream(pipeline, :sink)
+    refute_sink_buffer(pipeline, :sink, _buffer, 0)
+    Testing.Pipeline.terminate(pipeline)
+  end
+
+  test "Limits playback to realtime with introduced initial latency" do
+    buffers = [
+      %Buffer{pts: 0, payload: 0},
+      %Buffer{pts: Time.milliseconds(100), payload: 1}
+    ]
+
+    spec = [
+      child(:src, %Testing.Source{output: Testing.Source.output_from_buffers(buffers)})
+      |> child(:realtimer, %Realtimer{latency: Time.milliseconds(200)})
+      |> child(:sink, Testing.Sink)
+    ]
+
+    pipeline = Testing.Pipeline.start_link_supervised!(spec: spec)
+
+    refute_sink_buffer(pipeline, :sink, _buffer, 190)
     assert_sink_buffer(pipeline, :sink, %Buffer{payload: 0})
     refute_sink_buffer(pipeline, :sink, _buffer, 90)
     assert_sink_buffer(pipeline, :sink, %Buffer{payload: 1}, 20)
