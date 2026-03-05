682a6e7fe0e3b57794181c1034cda9e2996410e9
noarkhh <jakub.pryc@gmail.com>
2026-03-05 11:54:25 +0100

    Add more test cases

--- test/membrane_realtimer_test.exs
+++ test/membrane_realtimer_test.exs
@@ -267,6 +267,68 @@ defmodule Membrane.RealtimerTest do
     test_scenario(instructions, assertions, Time.milliseconds(700))
   end
 
+  test "Correctly reacts to stream lagging multiple times" do
+    instructions =
+      Enum.flat_map(0..35//6, fn seq ->
+        [
+          buffer: {seq, seq * 200},
+          sleep: 200,
+          buffer: {seq + 1, (seq + 1) * 200},
+          sleep: 200,
+          buffer: {seq + 2, (seq + 2) * 200},
+          sleep: 600,
+          buffer: {seq + 3, (seq + 3) * 200},
+          buffer: {seq + 4, (seq + 4) * 200},
+          buffer: {seq + 5, (seq + 5) * 200},
+          sleep: 200
+        ]
+      end)
+
+    assertions =
+      [refute: 690] ++
+        Enum.flat_map(0..35, fn seq ->
+          [
+            assert: {seq, 20},
+            refute: 190
+          ]
+        end)
+
+    test_scenario(instructions, assertions, Time.milliseconds(700))
+  end
+
+  test "Correctly reacts to multiple reset events" do
+    instructions =
+      Enum.flat_map(0..23//4, fn seq ->
+        [
+          buffer: {seq, seq * 3 * 200},
+          sleep: 200,
+          buffer: {seq + 1, (seq * 3 + 1) * 200},
+          sleep: 200,
+          buffer: {seq + 2, (seq * 3 + 2) * 200},
+          sleep: 200,
+          buffer: {seq + 3, (seq * 3 + 3) * 200},
+          event: :reset
+        ]
+      end)
+      |> IO.inspect()
+
+    assertions =
+      [refute: 590] ++
+        Enum.flat_map(0..23//4, fn seq ->
+          [
+            assert: {seq, 20},
+            refute: 190,
+            assert: {seq + 1, 20},
+            refute: 190,
+            assert: {seq + 2, 20},
+            refute: 190,
+            assert: {seq + 3, 20}
+          ]
+        end)
+
+    test_scenario(instructions, assertions, Time.milliseconds(600))
+  end
+
   defp test_scenario(instructions, assertions, max_latency) do
     spec =
       child(:src, %Membrane.Realtimer.TimedSource{instructions: instructions})
