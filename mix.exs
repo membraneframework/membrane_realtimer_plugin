b87cfcdc34ac4ed60bace16b448e91774ca2939e
noarkhh <jakub.pryc@gmail.com>
2026-03-12 16:30:02 +0100

    Bump version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.Realtimer.Plugin.Mixfile do
   use Mix.Project
 
-  @version "0.10.1"
+  @version "0.11.0"
   @github_url "https://github.com/membraneframework/membrane_realtimer_plugin"
 
   def project do
